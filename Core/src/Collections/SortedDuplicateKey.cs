870c2c0a81e385a5fd51e166aa23bc200b69856a
kzu <daniel@cazzulino.com>
2002-10-17 06:25:24 +0000

    Rollback Change

--- Core/src/Collections/SortedDuplicateKey.cs
+++ Core/src/Collections/SortedDuplicateKey.cs
@@ -60,10 +60,8 @@ namespace NMatrix.Core.Collections
 		{
       SortedDuplicateKey key = obj as SortedDuplicateKey;
       
-      //COmpatible Change
-      
 			// If the passed call order is MaxValue or is equal to us, put it above us.
-			if (key.Order > int.MaxValue || key.Order == _order || key.Order > _order)
+			if (key.Order == int.MaxValue || key.Order == _order || key.Order > _order)
 				return -1;
 			// If the order is less, it goes before us.
 			else if (key.Order < _order)
