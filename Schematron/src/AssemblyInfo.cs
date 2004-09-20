594dade41a50795e2908591c499a5a72dd3d6b30
kzu <daniel@cazzulino.com>
2004-09-20 03:44:20 +0000

    Updated code and samples for webservices support.

--- Schematron/src/AssemblyInfo.cs
+++ Schematron/src/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("NMatrix Project")]
 [assembly: AssemblyProduct("Schematron.NET")]
 
-[assembly: AssemblyVersion("0.53.*")]
+[assembly: AssemblyVersion("0.56.*")]
 
 [assembly: AssemblyDelaySign(false)]
 //TODO: Uncomment and sign before distribution.
